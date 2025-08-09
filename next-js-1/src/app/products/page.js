dfc3372cefc92b28073583f68c736517fefa950d
Poorvi <pjnayak2003@gmail.com>
2025-08-09 11:57:23 +0530

    Routes and client/server components

---
+++ next-js-1/src/app/products/page.js
@@ -0,0 +1,7 @@
+// each folder within app is a nested route. For it to render each folder needs to have a file called page.js
+
+export default function Products(){
+    return (
+        <h1>Welcome to Our Products Page</h1>
+    )
+}
